1bf7f4d0dc8bb1082f7d20fb61b2b3e14095e4c0
Aniket Teltumbade <teltumbade.s.aniket@gmail.com>
2020-07-07 20:59:36 +0530

    HTML & Desktop SCSS Coding Challenge - Frontendmentor

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+//To create a shortened link via the API
+/* 
+POST https://rel.ink/api/links/
+{
+  "url": "https://news.ycombinator.com/"
+}
+*/
+//To read shortened link details via the API
+/* 
+GET https://rel.ink/api/links/Nn8y9p/
+{
+  "hashid":"Nn8y9p",
+  "url": "https://news.ycombinator.com/",
+  "created_at":"2019-06-18T21:29:57.922801Z"
+}
+ */            
